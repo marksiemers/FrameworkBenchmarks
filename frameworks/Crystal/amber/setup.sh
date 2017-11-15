5b0a1db862e866bc7cb5a02e91b3b1f5d00fd312
Mark Siemers <mark.siemers@gmail.com>
2017-11-14 23:24:26 -0800

    Use half CPUs for amber and kemal

--- frameworks/Crystal/amber/setup.sh
+++ frameworks/Crystal/amber/setup.sh
@@ -6,17 +6,16 @@ shards update
 
 crystal build --release --no-debug src/amber.cr
 
-export GC_MARKERS=1
+export CPU_COUNT=$(nproc --all)
+export HALF_CPU_COUNT=$((((CPU_COUNT - 1) / 2) + 1))
+
+export GC_MARKERS=$HALF_CPU_COUNT
 
 export AMBER_ENV=production
 
-export CPU_COUNT=$(nproc --all)
-export CPU_MULTIPLIER=2
-export CONN_POOL_SIZE=$(($CPU_COUNT*$CPU_MULTIPLIER))
-export DB_URL_PARAMS="max_pool_size=$CONN_POOL_SIZE&max_idle_pool_size=$CONN_POOL_SIZE"
-export DATABASE_URL="postgres://benchmarkdbuser:benchmarkdbpass@TFB-database/hello_world?$DB_URL_PARAMS"
+export DATABASE_URL="postgres://benchmarkdbuser:benchmarkdbpass@TFB-database/hello_world"
 
-for i in $(seq 1 $(nproc --all)); do
+for i in $(seq 1 $HALF_CPU_COUNT); do
   ./amber &
 done
 
--- frameworks/Crystal/kemal/setup-postgres.sh
+++ frameworks/Crystal/kemal/setup-postgres.sh
@@ -6,11 +6,14 @@ shards install
 
 crystal build --release --no-debug server-postgres.cr
 
-export GC_MARKERS=1
+export CPU_COUNT=$(nproc --all)
+export HALF_CPU_COUNT=$((((CPU_COUNT - 1) / 2) + 1))
+
+export GC_MARKERS=$HALF_CPU_COUNT
 
 export KEMAL_ENV=production
 
-for i in $(seq 1 $(nproc --all)); do
+for i in $(seq 1 $HALF_CPU_COUNT); do
   ./server-postgres -p 8080 &
 done
 
