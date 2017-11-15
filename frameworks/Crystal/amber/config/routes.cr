7c5149772c62db5b3526047dc4a8d21ceb70b627
Faustino Aguilar <faustinoaq@users.noreply.github.com>
2017-11-15 16:37:10 -0500

    Add fast pipe

--- frameworks/Crystal/amber/config/routes.cr
+++ frameworks/Crystal/amber/config/routes.cr
@@ -1,13 +1,12 @@
 Amber::Server.configure do |app|
   pipeline :web do
+    Amber::Pipe::Fast.new
   end
 
   routes :web do
     get "/db", BenchmarkController, :db
-    get "/json", BenchmarkController, :json
     get "/queries", BenchmarkController, :queries
     get "/updates", BenchmarkController, :updates
     get "/fortunes", BenchmarkController, :fortunes
-    get "/plaintext", BenchmarkController, :plaintext
   end
 end
