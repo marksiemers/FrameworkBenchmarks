e0141bb9436497d79ca01defd6541b08a2ac40f3
Faustino Aguilar <faustinoaq@users.noreply.github.com>
2017-11-15 16:45:35 -0500

    Add context_with macro to avoid boilerplate

--- frameworks/Crystal/amber/src/pipes/fast.cr
+++ frameworks/Crystal/amber/src/pipes/fast.cr
@@ -1,13 +1,18 @@
 class Amber::Pipe::Fast < Amber::Pipe:Base
+  macro context_with(content_type)
+    context.response.status_code = 200
+    context.response.headers["Server"] = "Amber"
+    context.response.headers["Date"] = Time.now.to_s
+    context.response.headers["Content-Type"] = {{content_type}}
+  end
+
   def call(context : HTTP::Server::Context)
     case context.request.path
     when "/json"
-      context.response.status_code = 200
-      context.response.headers["Content-Type"] = "application/json"
+      context_with "application/json"
       {message: "Hello, World!"}.to_json(context.response)
     when "/plaintext"
-      context.response.status_code = 200
-      context.response.headers["Content-Type"] = "text/plain"
+      context_with "text/plain"
       context.response.print "Hello, World!"
     else
       call_next(context)
